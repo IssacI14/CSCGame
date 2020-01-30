a96f42132137fb56aa2d1d3015d26b7c8129ce9b
IssacI14 <springrex37@gmail.com>
2020-01-30 18:19:30 -0500

    Made a new project file cause i messed up on something on the other one so if anyone wants to figure it out go ahead

--- Monster Hunter Jeff/Assets/Scripts/Player Scripts/ContextClue.cs
+++ Monster Hunter Jeff/Assets/Scripts/Player Scripts/ContextClue.cs
@@ -5,16 +5,14 @@ using UnityEngine;
 public class ContextClue : MonoBehaviour {
 
     public GameObject contextClue;
-    public bool contextActive = false;
 
-    public void ChangeContext()
+    public void Enable()
     {
-        contextActive = !contextActive;
-        if (contextActive)
-        {
-            contextClue.SetActive(true);
-        }else{
-            contextClue.SetActive(false);
-        }
+        contextClue.SetActive(true);   
+    }
+
+    public void Disable()
+    {
+        contextClue.SetActive(false);
     }
 }
