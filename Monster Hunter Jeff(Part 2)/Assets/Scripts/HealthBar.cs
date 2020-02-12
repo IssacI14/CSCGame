d9595be402a6376f2d53cc420342830e122aa878
IssacI14 <springrex37@gmail.com>
2020-02-11 21:23:38 -0500

    Gave up on the health bar and added hit boxes

--- Monster Hunter Jeff(Part 2)/Assets/Scripts/HealthBar.cs
+++ Monster Hunter Jeff(Part 2)/Assets/Scripts/HealthBar.cs
@@ -5,18 +5,30 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    public PlayerHealth playerHealth;
+    public Image fillImage;
+    private Slider slider;
 
-    public Slider slider;
-    
-    public void SetMaxHealth(int health)
+
+    void Awake()
     {
-        slider.maxValue = health;
-        slider.value = health;
+        slider = GetComponent<Slider>();
     }
 
-    public void SetHealth(int health)
+    // Update is called once per frame
+    void Update()
     {
-        slider.value = health;
+        if(slider.value <= slider.minValue)
+        {
+            fillImage.enabled = false;
+        }
+
+        if(slider.value > slider.minValue && !fillImage.enabled)
+        {
+            fillImage.enabled = true;
+        }
+
+        float fillValue = playerHealth.currentHealth / playerHealth.maxHealth;
+        slider.value = fillValue;
     }
-    
 }
