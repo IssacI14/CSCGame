7c2a52552b65112afeae14f6445698e115edcfe4
IssacI14 <issaci2004@gmail.com>
2020-03-30 17:36:00 -0400

    Added a Coin system

---
+++ Monster Hunter Jeff(Part 2)/Assets/Scripts/Game Stuff/CointextManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CointextManager : MonoBehaviour
+{
+
+    public Inventory playerInventory;
+    public TextMeshProUGUI coinDisplay;
+
+    public void UpdateCoinCount()
+    {
+        coinDisplay.text = "" + playerInventory.coins;
+    }
+}
