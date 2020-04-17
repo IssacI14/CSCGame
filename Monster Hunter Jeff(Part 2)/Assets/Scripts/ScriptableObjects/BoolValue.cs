a3753dacdf7231da6686816d84254ec03dacb0a0
IssacI14 <springrex37@gmail.com>
2020-04-17 17:08:34 -0400

    Made some changes to the chest.
    made it so the chest stays open when leaving and comeing back to the scene

---
+++ Monster Hunter Jeff(Part 2)/Assets/Scripts/ScriptableObjects/BoolValue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class BoolValue : ScriptableObject, ISerializationCallbackReceiver
+{
+    public bool initalValue;
+
+    [HideInInspector]
+    public bool RuntimeValue;
+
+    public void OnAfterDeserialize()
+    {
+        RuntimeValue = initalValue;
+    }
+
+    public void OnBeforeSerialize() { }
+}
